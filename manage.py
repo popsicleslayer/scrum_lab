c0f5ff58ce50b5002b4d2998605e2134a6426442
popsicleslayer <70440489+popsicleslayer@users.noreply.github.com>
2021-05-18 18:23:24 +0200

    Fix if statment
    Rewritten to its original version.

--- manage.py
+++ manage.py
@@ -3,10 +3,6 @@ import os
 import sys
 
 if __name__ == '__main__':
-
-# nowy komenatrz
-
-if
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrumlab.settings')
     try:
         from django.core.management import execute_from_command_line

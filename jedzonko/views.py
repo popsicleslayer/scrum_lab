25fc2b73e9e2698b3bfa771e5ee197274488573c
Adam Puchejda <32865416+adampuchejda@users.noreply.github.com>
2021-05-19 15:59:09 +0200

    2.1/dashboard: dashboard view with associated main/ url created

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from django.http import HttpResponse
-
 from django.shortcuts import render
 from django.views import View
 
@@ -11,13 +10,10 @@ class IndexView(View):
         ctx = {"actual_date": datetime.now()}
         return render(request, "test.html", ctx)
 
-# szymon :)
-# test
-# Example
-
-# First view - MS
-
 
 def index_site(request):
-    return render(request,"index.html")
+    return render(request, "index.html")
 
+
+def dashboard(request):
+    return render(request, "dashboard.html")

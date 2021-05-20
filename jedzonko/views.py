027f68611c5fa0895b2f2cf4658e7e91157caa7e
sciesla4 <sciesla4@gmail.com>
2021-05-20 19:58:10 +0200

    26-22_&_32-41

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+from jedzonko.models import *
+
 
 class IndexView(View):
 
@@ -23,3 +25,38 @@ def index_site(request):
 
 def dashboard(request):
     return render(request, "dashboard.html")
+
+class ReceipeAddView(View):
+    def get(self,request):
+        return render(request, 'app-add-recipe.html')
+
+    def post(self,request):
+        recipe_name = request.POST.get('receipe_name')
+        recipe_description = request.POST.get('receipe_description')
+        time_of_preparing = request.POST.get('time_of_preparing')
+        way_of_preparing = request.POST.get('way_of_preparing')
+        ingredients = request.POST.get('ingredients')
+
+        Recipe.objects.create(name=recipe_name, description = recipe_description,preparation_time= time_of_preparing, ingredients= ingredients )
+
+        return render(request, "app-add-recipe.html")
+
+class RecipeModifyView(View):
+    def get(self, reqest, id):
+        return HttpResponse(f"Działa id:{id}")
+
+class PlanIdView(View):
+    def get(self, request, id):
+        return HttpResponse(f"Działa id: {id}")
+
+class PlanAddView(View):
+    def get(self,request):
+        return HttpResponse("Dodajmy nowy plan")
+
+class PlanAddReceipeView(View):
+    def get(self, request):
+        return HttpResponse("Dodajmy nowy przepis do planu")
+
+class PlanListView(View):
+    def get(self,request):
+        return HttpResponse("Tutaj będzie lista wszystkich planów")

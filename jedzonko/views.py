a02b295de5aef98953538a0f164cce936ae156cf
sciesla4 <sciesla4@gmail.com>
2021-05-27 13:52:24 +0200

    urls add , bootons href change

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -21,5 +21,42 @@ def index_site(request):
     return render(request, "index.html")
 
 
-def dashboard(request):
-    return render(request, "dashboard.html")
+class DashboardView(View):
+    def get(self,request):
+        return render(request, "dashboard.html")
+
+class RecipeListView(View):
+    def get(self,request):
+        return render(request, 'app-recipes.html')
+
+class RecipeAddView(View):
+    def get(self,request):
+        return render(request, 'app-add-recipe.html')
+
+class RecipeModifyView(View):
+    def get(self,request,id):
+        return HttpResponse(f"Działa id:{id}")
+
+class RecipeDetails(View):
+    def get(self,request, id):
+        return render(request, app-recipe-details.html)
+
+class PlanIdView(View):
+    def get(self,request,id):
+        return HttpResponse(f"Działa id: {id}")
+
+class PlanAddView(View):
+    def get(self,request):
+        return HttpResponse("Dodajmy nowy plan")
+
+class PlanAddReceipeView(View):
+    def get(self, request):
+        return HttpResponse("Dodajmy nowy przepis do planu")
+
+class PlanListView(View):
+    def get(self,request):
+        return render(request, 'app-schedules.html')
+
+class PlanListView(View):
+    def get(self, request):
+        return HttpResponse('lista planów')

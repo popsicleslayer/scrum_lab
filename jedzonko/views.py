82d6fba078824feb3c7f91657ea19b944f398922
popsicleslayer <70440489+popsicleslayer@users.noreply.github.com>
2021-05-30 15:28:18 +0200

    Add post method to PlanAddReceipeView. Correct excess classes with the same name in views.py Add name tags to form fields in app-schedules-meal-recipe.html Add models to admin.py

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -78,18 +78,6 @@ class PlanAddView(View):
         return HttpResponse(f'{planName}, {planDescription}')
 
 
-class RecipeListView(View):
-    def get(self,request):
-        return render(request, 'app-recipes.html')
-
-class RecipeAddView(View):
-    def get(self,request):
-        return render(request, 'app-add-recipe.html')
-
-class RecipeModifyView(View):
-    def get(self,request,id):
-        return HttpResponse(f"Działa id:{id}")
-
 class PlanAddReceipeView(View):
     def get(self, request):
         plans = Plan.objects.all()
@@ -101,19 +89,33 @@ class PlanAddReceipeView(View):
             'days': days,
         }
         return render(request, template_name='app-schedules-meal-recipe.html', context=ctx)
+
     def post(self, request, *args, **kwargs):
-        plan_id = request.POST.get('choosePlan')
-        recipe_id = request.POST.get('recipie')
-        day_name_id = request.POST.get('day')
-        order = request.POST.get('number')
-        meal_name = request.POST.get('name')
+        plan_id = request.POST.get('choose_plan')
+        recipe_id = request.POST.get('recipe')
+        day_name_id = request.POST.get('day_name')
+        order = request.POST.get('order')
+        meal_name = request.POST.get('meal_name')
+        recipe = Recipe.objects.get(pk=recipe_id)
+        plan = Plan.objects.get(pk=plan_id)
+        day_name = Dayname.objects.get(pk=day_name_id)
         if all([plan_id, recipe_id, day_name_id, order, meal_name]):
-            RecipePlan.objects.create(meal_name=meal_name, order=order,
-                                     day_name_id=day_name_id, recipe_id=recipe_id, plan_id=plan_id)
+            object = RecipePlan.objects.create(meal_name=meal_name, order=order, recipe=recipe, plan=plan)
+            object.save()
+            object.day_name.add(day_name)
             return redirect(f'/plan/{plan_id}')
         else:
             message = 'Proszę wypełnić wszystkie pola'
-        return render(request, template_name='app-schedules-meal-recipe.html', context={'message': message})
+            plans = Plan.objects.all()
+            recipes = Recipe.objects.all()
+            days = Dayname.objects.all()
+            ctx = {
+                'plans': plans,
+                'recipes': recipes,
+                'days': days,
+                'message': message,
+            }
+        return render(request, template_name='app-schedules-meal-recipe.html', context=ctx)
 
 
 class PlanListView(ListView):
